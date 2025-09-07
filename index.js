68cf34014ecae3356fef25950f7ec9e539faeeaf
Cain Tapp <cainlucastapp@gmail.com>
2025-09-06 18:23:16 -0700

    Added JS menu

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Write your solution in this file!
+// Global scope variables
+const burgers = ['Hamburger', 'Cheeseburger'];
+let featuredDrink = 'Strawberry Milkshake';
+
+// Function to add a new burger
+function addBurger() {
+  const newBurger = 'Flatburger';
+  burgers.push(newBurger);
+}
+
+// Always-true if statement
+if(true) {
+  const anotherNewBurger = 'Maple Bacon Burger';
+  burgers.push(anotherNewBurger);
+}
+
+// Function to change the featured drink
+function changeFeaturedDrink() {
+  featuredDrink = 'The JavaShake';
+}
